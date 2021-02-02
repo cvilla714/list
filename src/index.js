4c048c35ee2746f6a3dfbb81cd73ea108dc824c4
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-02 14:59:46 -0500

    added databse collection and insert records from the form

--- src/index.js
+++ src/index.js
@@ -1,24 +1,70 @@
+const lista = document.querySelector("ul");
+const form = document.querySelector(".add");
+const additem = (item) => {
+  let time = item.created_at.toDate();
+  let html = `
+  <li>
+  <div>${item.title}</div>
+  <div>${time}</div>
+  <i class="far fa-trash-alt delete"></i>
+  </li>
+  `;
+  // console.log(html);
+  lista.innerHTML += html;
+};
+
+db.collection("default-list")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((item) => {
+      console.log(item.data());
+      // console.log(item.data());
+      additem(item.data());
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const now = new Date();
+  const recipe = {
+    title: form.recipe.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+  db.collection("default-list")
+    .add(recipe)
+    .then(() => {
+      console.log("item added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 const addform = document.querySelector(".add");
 const list = document.querySelector(".todos");
 const search = document.querySelector(".search input");
 const generatetemplate = (todo) => {
   const html = `
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-        	<span>${todo}</span>
-        	<i class="far fa-trash-alt delete"></i>
-        </li>`;
+<lit class="list-group-item d-flex justify-content-between align-items-center">
+<span>${todo}</span>
+<i class="far fa-trash-alt delete"></i>
+</li>`;
   list.innerHTML += html;
 };
 
-addform.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const todo = addform.add.value.trim();
-  console.log(todo);
-  if (todo.length) {
-    generatetemplate(todo);
-    addform.reset();
-  }
-});
+// addform.addEventListener("submit", (event) => {
+// event.preventDefault();
+// const todo = addform.add.value.trim();
+// console.log(todo);
+// if (todo.length) {
+// generatetemplate(todo);
+// addform.reset();
+// }
+// });
 
 list.addEventListener("click", (event) => {
   if (event.target.classList.contains("delete")) {
