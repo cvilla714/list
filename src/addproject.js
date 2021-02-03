7a73d199eec2501e650580f91a430625ea6ced83
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-03 16:28:42 -0500

    append the projects to the dom

--- src/addproject.js
+++ src/addproject.js
@@ -6,3 +6,22 @@ project.addEventListener("click", (e) => {
     console.log("you are here");
   }
 });
+
+const data = document.querySelector(".addproject");
+
+data.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+  console.log(addtheproject.value);
+  const place = document.querySelector(".addingproject");
+  let glass = addtheproject.value;
+  const mas = document.createElement("h2");
+  mas.textContent = glass;
+  place.append(mas);
+  document.querySelector(".btn-close").click(); //this one automatically closes the modal
+});
+
+const close = document.querySelector(".btn-close");
+close.addEventListener("click", (e) => {
+  console.log(e);
+});
