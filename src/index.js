bf66c894f53f2bae0ecb3031dff86c1efac21e54
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-03 22:45:08 -0500

    extracted the code for the adding projects to the corresponsing file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-// import "../dist/css/style.css";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import "./addproject";
 import "./style.scss";
 
 import { foo } from "./date";
 foo();
-// const place = document.querySelector(".addingproject");
-const lista = document.querySelector("ul");
+
+// const lista = document.querySelector("ul");
 const list = document.querySelector(".todos");
 const search = document.querySelector(".search input");
 const form = document.querySelector(".add");
@@ -21,22 +20,7 @@ const additem = (item, id) => {
   </li>
   `;
   // console.log(html);
-  lista.innerHTML += html;
-};
-
-const addtheproject = (item, id) => {
-  const when = dateFns.distanceInWordsToNow(item.created_at.toDate(), { addSuffix: true });
-  // let time = item.created_at.toDate();
-  let html = `
-  <li data-id="${id}" class="list-group-item d-flex justify-content-between align-items-center">
-  <div>${item.title}</div>
-  <div>${when}</div>  
-  <i class="far fa-trash-alt delete"></i>
-  </li>
-  `;
-  // console.log(html);
-  // lista.innerHTML += html;
-  place.innerHTML += html;
+  list.innerHTML += html;
 };
 
 //get documents
@@ -54,20 +38,6 @@ const addtheproject = (item, id) => {
 // console.log(err);
 // });
 
-db.collection("default-projects")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((item) => {
-      console.log(item.id);
-      console.log(item.data());
-
-      addtheproject(item.data(), item.id);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 //real time event listners to the databse
 //to add and delete elements from the webbrowser
 db.collection("default-list").onSnapshot((snapshot) => {
@@ -83,7 +53,7 @@ db.collection("default-list").onSnapshot((snapshot) => {
   });
 });
 
-//delete document
+//delete document from the browser
 const deleteitem = (id) => {
   const totalitems = document.querySelectorAll("li");
   totalitems.forEach((itm) => {
@@ -114,7 +84,7 @@ form.addEventListener("submit", (e) => {
 });
 
 //deleting data from the database
-lista.addEventListener("click", (e) => {
+list.addEventListener("click", (e) => {
   console.log(e);
   if (e.target.classList.contains("delete")) {
     const id = e.target.parentElement.getAttribute("data-id");
