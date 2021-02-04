c95eb31e89449f9ed11c5bae107030e98d36b695
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-03 22:44:09 -0500

    added the functions for adding new projects and deleting from firebase and the webbrowser

--- src/addproject.js
+++ src/addproject.js
@@ -1,6 +1,7 @@
-const lista = document.querySelector(".projects");
+// const lista = document.querySelector(".projects");
 const project = document.querySelector(".btn-success");
-const place = document.querySelector(".addingproject");
+const place = document.querySelector(".projectos");
+const projectform = document.querySelector(".addproject");
 
 //get the correct content from the button
 project.addEventListener("click", (e) => {
@@ -14,17 +15,17 @@ const data = document.querySelector(".addproject");
 
 //adding the projects to the dom
 //and getting the input values from the form
-data.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(e);
-  console.log(addtheproject.value);
-  const place = document.querySelector(".addingproject");
-  let glass = addtheproject.value;
-  const mas = document.createElement("h2");
-  mas.textContent = glass;
-  place.append(mas);
-  document.querySelector(".btn-close").click(); //this one automatically closes the modal
-});
+// data.addEventListener("submit", (e) => {
+// e.preventDefault();
+// console.log(e);
+// console.log(addtheproject.value);
+// const place = document.querySelector(".addingproject");
+// let glass = addtheproject.value;
+// const mas = document.createElement("li");
+// mas.textContent = glass;
+// place.append(mas);
+// document.querySelector(".btn-close").click(); //this one automatically closes the modal
+// });
 
 //get the button action to close
 const close = document.querySelector(".btn-close");
@@ -39,12 +40,11 @@ including.addEventListener("click", (e) => {
 });
 
 const addtheproject = (item, id) => {
-  const when = dateFns.distanceInWordsToNow(item.created_at.toDate(), { addSuffix: true });
+  // const when = dateFns.distanceInWordsToNow(item.created_at.toDate(), { addSuffix: true });
   // let time = item.created_at.toDate();
   let html = `
   <li data-id="${id}" class="list-group-item d-flex justify-content-between align-items-center">
   <div>${item.title}</div>
-  
   <i class="far fa-trash-alt delete"></i>
   </li>
   `;
@@ -53,20 +53,23 @@ const addtheproject = (item, id) => {
   place.innerHTML += html;
 };
 
-db.collection("default-projects")
-  .get()
-  .then((snapshot) => {
-    snapshot.docs.forEach((item) => {
-      console.log(item.id);
-      console.log(item.data());
+//this is the one time snaption of the database
+// db.collection("default-projects")
+// .get()
+// .then((snapshot) => {
+// snapshot.docs.forEach((item) => {
+// console.log(item.id);
+// console.log(item.data());
+//
+// addtheproject(item.data(), item.id);
+// });
+// })
+// .catch((err) => {
+// console.log(err);
+// });
 
-      addtheproject(item.data(), item.id);
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
+//real time event listners to the databse
+//to add and delete elements from the webbrowser
 db.collection("default-projects").onSnapshot((snapshot) => {
   snapshot.docChanges().forEach((change) => {
     // console.log(change);
@@ -80,17 +83,66 @@ db.collection("default-projects").onSnapshot((snapshot) => {
   });
 });
 
-//deleting data from the database
-// lista.addEventListener("click", (e) => {
-// console.log(e);
-// if (e.target.classList.contains("delete")) {
-// const id = e.target.parentElement.getAttribute("data-id");
-// console.log(id);
 // db.collection("default-projects")
-// .doc(id)
-// .delete()
-// .then(() => {
-// console.log("item deleted");
+// .get()
+// .then((snapshot) => {
+// snapshot.docs.forEach((item) => {
+// console.log(item.id);
+// console.log(item.data());
+//
+// addtheproject(item.data(), item.id);
 // });
-// }
+// })
+// .catch((err) => {
+// console.log(err);
+// });
+
+//delete document from the browser
+const deleteitem = (id) => {
+  const totalitems = document.querySelectorAll("li");
+  totalitems.forEach((itm) => {
+    if (itm.getAttribute("data-id") === id) {
+      itm.remove();
+    }
+  });
+};
+
+//add documents to the database
+projectform.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const now = new Date();
+  const addtheproject = {
+    title: projectform.addtheproject.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+  db.collection("default-projects")
+    .add(addtheproject)
+    .then(() => {
+      console.log("item added");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  projectform.reset();
+  document.querySelector(".btn-close").click();
+});
+
+//deleting data from the database
+place.addEventListener("click", (e) => {
+  console.log(e);
+  if (e.target.classList.contains("delete")) {
+    const id = e.target.parentElement.getAttribute("data-id");
+    console.log(id);
+    db.collection("default-projects")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("item deleted");
+      });
+  }
+});
+
+// place.addEventListener("click", (e) => {
+// console.log(e);
 // });
