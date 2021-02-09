30603d4d1cf8de704a4c954772048583173ba81a
Cosmel Villalobos <cosmelvg@gmail.com>
2021-02-09 02:15:54 -0500

    added counter and crossing lines for tasks completed

--- src/list.js
+++ src/list.js
@@ -9,6 +9,8 @@ const listTitleElement = document.querySelector("[data-list-title]");
 const listCountElement = document.querySelector("[data-list-count");
 const tasksContainer = document.querySelector("[data-tasks]");
 const tasksTemplate = document.querySelector("#task-template");
+const newTaskForm = document.querySelector("[data-new-task-form]");
+const newTaskInput = document.querySelector("[data-new-task-input]");
 
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
@@ -19,6 +21,16 @@ listContainer.addEventListener("click", (e) => {
   if (e.target.tagName.toLowerCase() === "li") {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
+  }
+});
+
+tasksContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "input") {
+    const selectedList = lists.find((list) => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find((task) => task.id === e.target.id);
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
   }
 });
 
@@ -38,17 +50,30 @@ newListForm.addEventListener("submit", (e) => {
   saveAndRender();
 });
 
+newTaskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName === "") return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
+
 function createList(name) {
   return {
     id: Date.now().toString(),
     name: name,
-    tasks: [
-      {
-        id: "adaf",
-        name: "Test",
-        complete: true,
-      },
-    ],
+    tasks: [],
+  };
+}
+
+function createTask(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    complete: false,
   };
 }
 
